1fd5d21082ad56b6cba189832a760528ed71ddad
Dorianliddell <DorianLiddell@gmail.com>
2025-09-29 18:57:37 +0300

    главная страница

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <a href="/sticker">Перейти на страницу стикера</a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  text-align: center;
+  padding-top: 50px;
+}
+
+a {
+  font-size: 18px;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border-radius: 5px;
+  &:hover {
+    background-color: #0056b3;
+  }
+}
+</style>
